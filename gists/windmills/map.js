cdb621bd5e981ddd39f20c93c03fbeca1ee32a23
Michael Bowman <bowmanmc@gmail.com>
2015-10-16 19:57:26 -0400

    first working draft. finally got the wind turbines on the map and rotating proportional to their output.

--- gists/windmills/map.js
+++ gists/windmills/map.js
@@ -3,6 +3,14 @@ function Map(elementIdSelector) {
 
     this.mapSelector = elementIdSelector;
 
+    this.WINDMILL_SVG = '<svg class="windmill" version="1.1" viewBox="0 0 512 512">' +
+        '<path id="tower" d="M279,459c-9,0-18,0-36,0s9-225,9-225h18C270,234,297,459,279,459z"/>' +
+        '<g id="propellers">' +
+        '<path d="M261,234c0,0-27-36-27-54s9-126,18-126s9,9,9,18C261,103.272,261,234,261,234z"/>' +
+        '<path d="M261,234c0,0-17.677,41.382-33.266,50.382s-113.619,55.206-118.119,47.411C105.115,324,112.91,319.5,120.704,315C147.786,299.363,261,234,261,234z"/>' +
+        '<path d="M261,234.055c0,0,44.677-5.382,60.266,3.618s104.619,70.794,100.119,78.589c-4.5,7.793-12.294,3.293-20.089-1.207C374.214,299.418,261,234.055,261,234.055z"/>' +
+        '<circle cx="261" cy="234" r="27"/></g></svg>';
+
     this.drawMap = function() {
 
         var map = this;
@@ -19,7 +27,7 @@ function Map(elementIdSelector) {
 
         map.path = d3.geo.path().projection(map.projection);
 
-        map.svg = d3.select(map.mapSelector).append('svg')
+        map.svg = d3.select(map.mapSelector).select('.base').append('svg')
             .attr('width', map.width)
             .attr('height', map.height);
 
@@ -98,6 +106,8 @@ function Map(elementIdSelector) {
         var minLon = map.bounds[0][0];
         var maxLon = map.bounds[1][0];
 
+        //var lyrData = d3.select(map.mapSelector).select('.data');
+        var SVG = '<svg class="marker" viewBox="0 0 120 120" version="1.1"><circle cx="60" cy="60" r="50"/></svg>';
         var ohioData = [];
         var i, dt, coords, lat, lon;
         var len = data.length;
@@ -109,6 +119,29 @@ function Map(elementIdSelector) {
             if ((lat >= minLat && lat <= maxLat) &&
                 (lon >= minLon && lon <= maxLon)) {
                 ohioData.push(dt);
+
+                var capacity = Number(dt.GeneratingCapacity.replace(' MW', ''));
+                var secs = 1 / capacity;
+
+                console.log('Farm: ' + dt.GeneratingCapacity + ' - ' + secs + 's');
+
+                var pos = map.projection([lon, lat]);
+                var posStr = 'left: ' + (pos[0] - 24) + '; top: ' + (pos[1] - 42) + ';';
+                // put an svg element in the right place...
+                /*$(SVG).attr({
+                    'width': '24px',
+                    'height': '24px',
+                    'style': posStr
+                }).appendTo(map.mapSelector + ' .data');
+                */
+                var windmill = $(map.WINDMILL_SVG);
+                $(windmill).attr({
+                    'style': posStr
+                });
+                $(windmill).find('#propellers').attr({
+                    'style': 'animation: spin ' + secs + 's linear infinite;'
+                });
+                windmill.appendTo(map.mapSelector + ' .data');
             }
         }
 
